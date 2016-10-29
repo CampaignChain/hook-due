2074ee2cfb5ee920eee7f609b9e5a4c522750460
Sandro Groganz <sandro@campaignchain.com>
2016-10-29 11:24:03 +0200

    CampaignChain/campaignchain#263 Use getManager() instead of getEntityManager() for Doctrine

--- EntityService/DueService.php
+++ EntityService/DueService.php
@@ -20,7 +20,7 @@ namespace CampaignChain\Hook\DueBundle\EntityService;
 use CampaignChain\CoreBundle\Entity\Hook;
 use CampaignChain\CoreBundle\EntityService\HookServiceTriggerInterface;
 use CampaignChain\Hook\DueBundle\Entity\Due;
-use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\Common\Inflector\Inflector;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -29,10 +29,10 @@ class DueService implements HookServiceTriggerInterface
     protected $em;
     protected $container;
 
-    public function __construct(EntityManager $em, ContainerInterface $container)
+    public function __construct(ManagerRegistry $managerRegistry, ContainerInterface $container)
     {
         $this->container = $container;
-        $this->em = $em;
+        $this->em = $managerRegistry->getManager();
     }
 
 //    public function newObject($entityClass, $entityId, $formData){
